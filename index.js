d20b3ce8da290f87d0037063c55f033ecc1a4967
x414e54 <x414e54@linux.com>
2020-12-21 20:14:43 +0000

    Add support for macos

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const ueroot = core.getInput('ueroot');
   const script = core.getInput('script');
   const target = core.getInput('target');
   const projectsroot = core.getInput('projectsroot');
   const outputdir = core.getInput('outputdir');
   const { exec, spawn } = require('child_process');
   
-  if (script.includes('\'') || script.includes('"') || target.includes('\'') || target.includes('"') || projectsroot.includes('\'') || projectsroot.includes('"') || outputdir.includes('\'') || outputdir.includes('"'))
+  if (ueroot.includes('\'') || ueroot.includes('"') || script.includes('\'') || script.includes('"') || target.includes('\'') || target.includes('"') || projectsroot.includes('\'') || projectsroot.includes('"') || outputdir.includes('\'') || outputdir.includes('"'))
   {
      core.setFailed("Invalid characters in string");
   }
   
-  // TODO this seems pretty insecure.
-  exec('"D:\\UnrealEngine\\Engine\\Build\\BatchFiles\\RunUAT.bat" BuildGraph -Script="' + script + '" -Target="' + target + '" -set:ProjectsRoot="' + projectsroot + '" -set:OutputDir="' + outputdir + '"', (err, stdout, stderr) => {
+  var uatpath = ueroot + "/Engine/Build/BatchFiles/RunUAT.sh";
+  if (process.platform === "win32")
+  {
+    uatpath = ueroot += "\\Engine\\Build\\BatchFiles\\RunUAT.bat"
+  }
+
+  exec('"' + uatpath + '" BuildGraph -Script="' + script + '" -Target="' + target + '" -set:ProjectsRoot="' + projectsroot + '" -set:OutputDir="' + outputdir + '"', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     if (err) {
